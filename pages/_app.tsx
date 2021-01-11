5a51895e799b71eeeda02259b575885a3c77d0ab
megawawa <71688636+megawawa@users.noreply.github.com>
2021-01-10 19:05:54 -0800

    Title: navbar and login
    Description:
    create navbar.
    add a simple login page:
     1. allow login with typed in user
     2. update logged in user
    
    Test plan: .

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import MainNavbar from '../components/navbar'
+import { AppProps } from 'next/app'
+import { ContextProvider } from '../components/login'
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+
+  return (
+    <ContextProvider>
+      <MainNavbar {...pageProps} />
+      <Component {...pageProps} />
+    </ContextProvider >
+  );
+}
+
+export default MyApp
---
+++ pages/login.tsx
@@ -0,0 +1,36 @@
+import { Component, useState } from 'react';
+import { LoginContext, LoginContextType, useLoginContext } from '../components/login'
+import { useRouter } from 'next/router'
+
+type NameFormType = {
+    name?: string
+}
+
+export default function NameForm() {
+    const [state, setState] = useState<NameFormType>({});
+    const router = useRouter();
+    const handleChange = (event) => {
+        setState({ name: event.target.value });
+    }
+
+    const handleSubmit = (update, event) => {
+        alert('A name was submitted: ' + state.name);
+        console.log(update);
+        update(state.name);
+        router.push('/');
+        event.preventDefault();
+    }
+
+    return (<LoginContext.Consumer>
+        {({ name, update }) => (
+            <form onSubmit={handleSubmit.bind(this, update)}>
+                <label>
+                    Name:
+                <input type="text" value={state.name ?? name ?? ''}
+                        onChange={handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        )}
+    </LoginContext.Consumer>);
+}
