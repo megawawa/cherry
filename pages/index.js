8b0938cf2c0cd6e93a46c581832f6d758c3969a4
megawawa <71688636+megawawa@users.noreply.github.com>
2021-01-19 22:04:26 -0800

    Title: [main page] add staticprops for intial page
    Description:
    as title
    
    Test plan: .

--- pages/index.js
+++ pages/index.js
@@ -95,3 +95,9 @@ export default function Home() {
     </>
   )
 }
+
+export async function getStaticProps(context) {
+  return {
+    props: {}, // will be passed to the page component as props
+  }
+}
