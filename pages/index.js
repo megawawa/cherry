2349593c9e5bc2873b29c387dbf9f9e762e802b1
megawawa <71688636+megawawa@users.noreply.github.com>
2021-02-24 02:36:28 -0800

    Title: [ui][home] change home page
    Description:
    
    Test plan:
    
    Task:

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Card, Button } from 'react-bootstrap'
+import { Card, Button, Jumbotron } from 'react-bootstrap'
 import styles from '../styles/Home.module.css'
 import { useSession } from 'next-auth/client'
 import MainAccountView from "../components/layout/mainAccountView";
@@ -19,85 +19,40 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <Image
-          layout="fill"
-          className="object-center object-cover pointer-events-none"
-          src={"/background.jpg"}
-          alt={"background"}
-        />
-        <div className={styles.grid}>
-          <Card className={styles.card}>
-            <Card.Body>
-              <Card.Title>I am a student</Card.Title>
-              <Card.Text>
-                Want to <span className={styles.highlightCardText}>
-                  gain more</span><span> knowledge</span>?
-                Create your profile to browse quizzes & tests, ask questions
-                online or find a tutor
-          </Card.Text>
-              <div className={styles.cardFooter}>
-                <Button variant="primary" href="/signup">Start Learning</Button>
-                <div className={styles.priceTag}>Free </div>
-              </div>
-
-            </Card.Body>
-          </Card>
-
-          <Card className={styles.card}>
-            <Card.Body>
-              <Card.Title>I am a tutor</Card.Title>
-              <Card.Text>
-                Want to <span className={styles.highlightCardText}>
-                  reach more</span><span> students</span>?
-                  Build up your profile by answering students' questions,
-                  creating your own problem sets, and contributing
-                  quizzes.
-              </Card.Text>
-              <div className={styles.cardFooter}>
-                <Button variant="primary" href="/signup">Start Tutoring</Button>
-                <div className={styles.priceTag}>Free </div>
-              </div>
-            </Card.Body>
-          </Card>
-
-          <Card className={styles.card}>
-            <Card.Body>
-              <Card.Title>Just browsing? </Card.Title>
-              <Card.Text>
-                No worries! <span className={styles.highlightCardText}>Explore</span>
-                <span> </span>
-                 quizzes, tests and tutors. Sign up when
+      <main className={styles.mainNew}>
+        <Jumbotron style={{ maxHeight: "25vh", minWidth: "100vw" }}>
+          <h1>Hello, Student</h1>
+          <p>
+            <span className={styles.highlightCardText}>Assess</span>
+            <span> </span>
+           yourself and learn through quizzes and tests. Get your questions answered -- for free
+          </p>
+          <p>
+            <Button variant="primary" href="/signup">Sign Up now</Button>
+          </p>
+        </Jumbotron>
+        <Jumbotron style={{ maxHeight: "25vh", minWidth: "100vw" }}>
+          <h1>Hello, Tutor</h1>
+          <p>
+            <span className={styles.highlightCardText}>Reach</span>
+            <span> </span> more students -- for free
+          </p>
+          <p>
+            <Button variant="primary" href="/signup">Sign Up now</Button>
+          </p>
+        </Jumbotron>
+        <Jumbotron style={{ maxHeight: "25vh", minWidth: "100vw" }}>
+          <h1>Just browsing?</h1>
+          <p>
+            <span className={styles.highlightCardText}>Explore</span>
+            <span> </span>
+                 quizzes, tests and tutors. Sign up as student/tutor when
                 you need more features!
-          </Card.Text>
-              <div className={styles.cardFooter}>
-                <Button variant="primary">Start exploring</Button>
-                <div className={styles.priceTag}> Free</div>
-              </div>
-            </Card.Body>
-          </Card>
-        </div>
-
-        <div className={styles.textContainer}>
-          <div>
-            <span>Welcome, </span>
-            <span>have you quizzed today?</span>
-          </div>
-          <div className={styles.infoContainer}>
-            <div className={styles.infoCard}>
-              <span className={styles.infoNumber}>10k+ </span>
-              <span className={styles.infoItem}>Problems</span>
-            </div>
-            <div className={styles.infoCard}>
-              <span className={styles.infoNumber}>100+ </span>
-              <span className={styles.infoItem}>Tutors</span>
-            </div>
-            <div className={styles.infoCard}>
-              <span className={styles.infoNumber}>1k+ </span>
-              <span className={styles.infoItem}>Students</span>
-            </div>
-          </div>
-        </div>
+          </p>
+          <p>
+            <Button variant="primary" href="/quizzes">Start exploring</Button>
+          </p>
+        </Jumbotron>
       </main>
     </>
   )
